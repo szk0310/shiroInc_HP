34784dc2c88af6814ad3bac24facac545408e139
鈴木隆弥 <suzuki@shiro-k.jp>
2026-02-24 23:07:12 +0900

    Final aggressive speed optimizations for carousel and text motion

--- to_pardot_HP/assets/js/common.js
+++ to_pardot_HP/assets/js/common.js
@@ -1,60 +1,60 @@
 $(function () {
 
-    /*--------------------------------
-      smoothscroll
-	 ---------------------------------*/
-    var headerHeight = $('header').innerHeight() - 1;
-    $('a[href^="#"]:not(.js-modal-open,.modal-close)').click(function () {
-        var speed = 700;
-        var href = $(this).attr("href");
-        var target = $(href === "#" || href === "" ? 'html' : href);
-        var position = target.offset().top - headerHeight;
-        $("html, body").animate({
-            scrollTop: position
-        }, speed, "swing");
-        return false;
-    });
+  /*--------------------------------
+    smoothscroll
+ ---------------------------------*/
+  var headerHeight = $('header').innerHeight() - 1;
+  $('a[href^="#"]:not(.js-modal-open,.modal-close)').click(function () {
+    var speed = 700;
+    var href = $(this).attr("href");
+    var target = $(href === "#" || href === "" ? 'html' : href);
+    var position = target.offset().top - headerHeight;
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
+  });
 
 });
 
 /*--------------------------------
       ハンバーガーmenu
-	 ---------------------------------*/
+   ---------------------------------*/
 
-$('#sp-menu-trigger').click(function(){
-    $("header").toggleClass('open');
-    $("header nav").slideToggle();
+$('#sp-menu-trigger').click(function () {
+  $("header").toggleClass('open');
+  $("header nav").slideToggle();
 });
 
 
-$('header nav li a ').click(function(){
-    if($(window).innerWidth() <= 900 ){
+$('header nav li a ').click(function () {
+  if ($(window).innerWidth() <= 900) {
     $('#sp-menu-trigger').trigger('click');
     $("header").removeClass('open');
-    } else{
-        ;
-    }
+  } else {
+    ;
+  }
 });
 
 
-$(window).on('load resize', function(){
+$(window).on('load resize', function () {
 
-		$("header").removeClass('open');
+  $("header").removeClass('open');
 
-    if($(window).innerWidth() <= 900 ){
-        //スマホの時の状態
-        $('#sp-menu-trigger').show();
-		$("header nav").hide();
-    } else{
-        //タブレット・PCの時の状態
-        $('#sp-menu-trigger').hide();
-		$("header nav").show();
-    }
-}); 
+  if ($(window).innerWidth() <= 900) {
+    //スマホの時の状態
+    $('#sp-menu-trigger').show();
+    $("header nav").hide();
+  } else {
+    //タブレット・PCの時の状態
+    $('#sp-menu-trigger').hide();
+    $("header nav").show();
+  }
+});
 
 /*--------------------------------
        modal
-	 ---------------------------------*/
+   ---------------------------------*/
 $(function () {
   $('.js-modal-open').on('click', function () {
     var target = $(this).data('target');
@@ -77,69 +77,69 @@ $(function () {
 
 /*--------------------------------
        textアニメーション
-	 ---------------------------------*/
+   ---------------------------------*/
 setInterval(() => {
-    $(".mv-copy.text-span").addClass('show');
-  }, 0);
+  $(".mv-copy.text-span").addClass('show');
+}, 0);
 
 setInterval(() => {
-    $(".mv-copy2.text-span.pc").addClass('show');
-  }, 600);
+  $(".mv-copy2.text-span.pc").addClass('show');
+}, 300);
 
 setInterval(() => {
-    $(".mv-copy2.text-span.sp").addClass('show');
-  }, 400);
+  $(".mv-copy2.text-span.sp").addClass('show');
+}, 200);
 setInterval(() => {
-    $(".mv-copy3.text-span.sp").addClass('show');
-  }, 800);
+  $(".mv-copy3.text-span.sp").addClass('show');
+}, 400);
 setInterval(() => {
-    $(".mv-copy4.text-span.sp").addClass('show');
-  }, 1200);
+  $(".mv-copy4.text-span.sp").addClass('show');
+}, 600);
 setInterval(() => {
-    $(".mv-copy5.text-span.sp").addClass('show');
-  }, 1600);
+  $(".mv-copy5.text-span.sp").addClass('show');
+}, 800);
 
 
 /*--------------------------------
        スクロールclass付与
-	 ---------------------------------*/
-const myFunc = function(){
-    //Classを追加する要素を取得
-    const target = document.getElementsByClassName('text-span');
-    const target1 = document.getElementsByClassName('border');
-    const target2 = document.getElementsByClassName('border2');
-    //Classを追加する位置を指定（ビューポート内）
-    
-    const position = Math.floor(window.innerHeight * .75); //左記はビューポート内の上から75%の位置を指定
-    
-    for (let i = 0; i < target.length; i++) {        
-        //要素の上部座標を取得する（小数点切り捨て）
-        let offsetTop = Math.floor(target[i].getBoundingClientRect().top);
-        //要素の上部座標がpositionの位置を通過したら
-        if (offsetTop < position) {
-            //要素にClassを追加する
-            target[i].classList.add('show');
-        }
+   ---------------------------------*/
+const myFunc = function () {
+  //Classを追加する要素を取得
+  const target = document.getElementsByClassName('text-span');
+  const target1 = document.getElementsByClassName('border');
+  const target2 = document.getElementsByClassName('border2');
+  //Classを追加する位置を指定（ビューポート内）
+
+  const position = Math.floor(window.innerHeight * .75); //左記はビューポート内の上から75%の位置を指定
+
+  for (let i = 0; i < target.length; i++) {
+    //要素の上部座標を取得する（小数点切り捨て）
+    let offsetTop = Math.floor(target[i].getBoundingClientRect().top);
+    //要素の上部座標がpositionの位置を通過したら
+    if (offsetTop < position) {
+      //要素にClassを追加する
+      target[i].classList.add('show');
     }
-    
-    for (let i = 0; i < target1.length; i++) {   
-        //要素の上部座標を取得する（小数点切り捨て）
-        let offsetTop = Math.floor(target1[i].getBoundingClientRect().top);
-        //要素の上部座標がpositionの位置を通過したら
-        if (offsetTop < position) {
-            //要素にClassを追加する
-            target1[i].classList.add('active');
-        }
+  }
+
+  for (let i = 0; i < target1.length; i++) {
+    //要素の上部座標を取得する（小数点切り捨て）
+    let offsetTop = Math.floor(target1[i].getBoundingClientRect().top);
+    //要素の上部座標がpositionの位置を通過したら
+    if (offsetTop < position) {
+      //要素にClassを追加する
+      target1[i].classList.add('active');
     }
-    for (let i = 0; i < target2.length; i++) {   
-        //要素の上部座標を取得する（小数点切り捨て）
-        let offsetTop = Math.floor(target2[i].getBoundingClientRect().top);
-        //要素の上部座標がpositionの位置を通過したら
-        if (offsetTop < position) {
-            //要素にClassを追加する
-            target2[i].classList.add('active');
-        }
+  }
+  for (let i = 0; i < target2.length; i++) {
+    //要素の上部座標を取得する（小数点切り捨て）
+    let offsetTop = Math.floor(target2[i].getBoundingClientRect().top);
+    //要素の上部座標がpositionの位置を通過したら
+    if (offsetTop < position) {
+      //要素にClassを追加する
+      target2[i].classList.add('active');
     }
+  }
 }
 //スクロールイベントリスナーに登録
 window.addEventListener('scroll', myFunc, false);
